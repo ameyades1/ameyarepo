97f33430f3676c8dd1551fdfbf5cdc72ba80b592
Ameya Deswandikar <ameya.des1@gmail.com>
2019-07-20 02:58:13 -0700

    Adding leftover bp results

--- workspace/mooc/everyday_scripting_with_ruby/course_files/code/inventory/difference.rb
+++ workspace/mooc/everyday_scripting_with_ruby/course_files/code/inventory/difference.rb
@@ -1,6 +1,21 @@
+unless ARGV.length == 2
+	puts "Usage: ruby difference.rb old_inventory.txt new_inventory.txt"
+	exit
+end
 
-old_inventory = File.open('old-inventory.txt').readlines
-new_inventory = File.open('new-inventory.txt').readlines
+def get_inventory(file)
+	File.open(file).readlines.collect do |line|
+		line.downcase
+	end
+end
+
+
+old_inventory = get_inventory(ARGV[0])
+new_inventory = get_inventory(ARGV[1])
+
+
+#old_inventory = File.open('old-inventory.txt').readlines
+#new_inventory = File.open('new-inventory.txt').readlines
 
 files_added = new_inventory - old_inventory
 
@@ -15,9 +30,8 @@ puts old_inventory - new_inventory
 puts
 
 puts "Following files are unchanged... \n"
+
 puts new_inventory - files_added
 puts "--- OR ---"
-puts old_inventory - files_removed.shuffle
-puts "--- OR ---"
-puts files_removed.shuffle
 
+puts old_inventory - files_removed.shuffle
